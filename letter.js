2ede2b39aefcdb92d969f15869b54de2f08f6d54
soyeonparkpark <43019722+soyeonparkpark@users.noreply.github.com>
2020-02-07 21:54:42 -0800

    Create letter.js

---
+++ letter.js
@@ -0,0 +1,263 @@
+function setup() {
+  createCanvas(600, 1000);
+}
+
+function printA(x,y){
+  
+  
+  line(x+50,y,x+50,y+100);
+  line(x+50,y,x,y+100);
+  line(x,y+50,x+60,y+50);
+  
+}
+function printB(x,y){
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  
+  line(x+40,y,x+40,y+100);
+  line(x,y+50,x+40,y+50);
+  line(x,y+100,x+40,y+100);
+}
+
+
+function printC (x,y){
+ 
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+ 
+    line(x,y+100,x+40,y+100);
+}
+  
+function printD (x,y){
+ 
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x,y,x+40,y);
+  line(x,y+100,x+40,y+100);
+  line(x+70,y+50,x+40,y+100);
+  line(x+70,y+50,x+40,y);
+}
+function printE(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y, x + 40, y);
+  line(x, y + 50, x + 40, y + 50);
+  line(x, y + 100, x + 40, y + 100);
+}
+function printF(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y, x + 40, y);
+  line(x, y + 50, x + 40, y + 50);
+  
+}
+function printG (x,y){
+ 
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x,y,x+40,y);
+  line(x,y+100,x+40,y+100);
+  line(x+70,y+100,x+40,y+100);
+  line(x+70,y+50,x+40,y);
+  line(x+70,y+100,x+70,y+60);
+  line(x+30,y+60,x+70,y+60);
+  
+}
+function printH(x, y) {
+  line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 100);
+  line(x, y + 50, x + 40, y + 50);
+}
+function printI (x,y){
+ 
+  
+  line(x+20,y,x+20,y+100);
+  line(x,y,x+40,y);
+ 
+    line(x,y+100,x+40,y+100);
+}
+
+function printJ(x,y){
+  
+  
+  line(x,y,x+45,y);
+  line(x+30,y,x+30,y+100);
+  line(x,y+100,x+30,y+100);
+  line(x,y+70,x,y+100);
+}
+
+function printK(x, y) {
+  line(x, y, x, y + 100);
+  line(x, y+50, x + 40, y);
+ 
+ line(x, y+50, x +40, y+100);
+ 
+}
+function printL(x,y){    
+ 
+   line(x,y,x,y+100);
+   line(x,y+100,x+40,y+100);
+}
+
+function printM(x,y){
+  
+  
+  line(x+50,y,x+50,y+100);
+  line(x+50,y,x,y+100);
+   line(x+100,y,x+50,y+100);
+  line(x+100,y,x+100,y+100);
+  
+  
+}
+
+function printN(x,y){
+  
+  
+  line(x+50,y,x+50,y+100);
+  
+   line(x+100,y,x+50,y+100);
+  line(x+100,y,x+100,y+100);
+  
+  
+}
+
+function printO(x,y){
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x+40,y,x+40,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printP(x,y){
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  
+  line(x+40,y,x+40,y+50);
+  line(x,y+50,x+40,y+50);
+  
+}
+function printQ(x,y){
+  
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x+40,y,x+40,y+100);
+  line(x,y+100,x+40,y+100);
+  line(x + 60, y+100, x + 20, y + 50);
+}
+
+
+
+function printR(x, y) {
+  line(x+40, y, x, y);
+    line(x, y, x, y + 100);
+  line(x + 40, y, x + 40, y + 50);
+  line(x, y + 50, x + 40, y + 50);
+  line(x + 70, y+100, x + 40, y + 50);
+}
+
+
+
+  function printS(x,y){
+  
+  line(x,y,x,y+50);
+  line(x,y,x+40,y);
+ 
+  line(x+40,y+50,x+40,y+100);
+  line(x+40,y,x+40,y+30);
+  line(x,y+50,x+40,y+50);
+  line(x,y+100,x+40,y+100);
+    line(x,y+100,x,y+70);
+}
+
+function printT (x,y){
+ 
+  
+  line(x+20,y,x+20,y+100);
+  line(x,y,x+40,y);
+}
+
+function printU(x,y){
+  
+  line(x,y,x,y+100);
+ 
+  line(x+40,y,x+40,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printV(x,y){
+  
+  line(x+30,y+100,x,y);
+  
+  line(x+60,y,x+30,y+100);
+ 
+}
+
+function printW(x,y){
+  
+  line(x+30,y+100,x,y);
+  line(x+60,y+100,x+30,y);
+  line(x+60,y,x+30,y+100);
+  line(x+90,y,x+60,y+100);
+ 
+}
+
+function printX(x,y){
+  
+
+  line(x+60,y+100,x+10,y);
+  line(x+60,y,x+10,y+100);
+ 
+ 
+}
+function printY(x,y){
+  
+
+  line(x+35,y+50,x+10,y);
+  line(x+60,y,x+10,y+100);
+ 
+ 
+}
+function printZ (x,y){
+ 
+  
+  line(x+40,y,x,y+100);
+  line(x,y,x+40,y);
+ 
+    line(x,y+100,x+40,y+100);
+}
+
+ function draw() {
+  background(220);
+    printA(50,50);
+    printB(150,50);
+   printC(250,50);
+   printD(350,50);
+   printE(450,50);
+    printF(50,200);
+    printG(150,200);
+   printH(250,200);
+   printI(350,200);  
+   printJ(450,200);
+  printK(50,350);
+   printL(150,350);
+   printM(250,350);
+   printN(350,350);
+   printO(500,350);
+    printP(50,500);
+     printQ(150,500);
+    printR(250,500);
+    printS(350,500);
+    printT(450,500);
+  printU(50,650);
+    printV(150,650);
+      printW(250,650);
+    printX(350,650);
+   printY(450,650);
+   printZ(250,800);
+  
+}
+
